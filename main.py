474142bce20e437e7442f9c0aa815ec64fab7770
Charlie <charlies.retro.computing@gmail.com>
2025-07-27 19:21:45 -0400

    v1.0.0

---
+++ main.py
@@ -0,0 +1,45 @@
+import modules.markov_loader as markov_loader
+import modules.syllables as syllables
+
+MARKOV_MODEL = markov_loader.get_markov_model()
+
+ANSI = "\033["
+GREEN = ANSI + "32m"
+RESET = ANSI + "0m"
+
+def print_with_border(lines: list[str]) -> None:
+    lines = [line.strip() for line in lines]
+    
+    if not lines:
+        return
+
+    leftspacing: str = " " * 5
+    max_len = max(len(line) for line in lines)
+    bt = leftspacing + "╔" + "═" * (max_len + 2) + "╗"
+    bb = leftspacing + "╚" + "═" * (max_len + 2) + "╝"
+
+    print(f"\n\n{leftspacing}{GREEN}{'  Your haiku is ready!'.center(max_len)}{RESET}")
+    print(bt)
+    for line in lines:
+        print(f"{leftspacing}║ {line.center(max_len)} ║")
+    print(bb + "\n\n")
+
+def generate_line(syllable_count):
+    for i in range(100):
+        sentence = MARKOV_MODEL.make_sentence(max_words=syllable_count*3)
+        if not sentence:
+            continue
+        words = sentence.split()
+        while words and syllables.get_line_syllables(" ".join(words)) > syllable_count:
+            words.pop()
+        line_candidate = " ".join(words)
+        if syllables.get_line_syllables(line_candidate) == syllable_count:
+            return line_candidate
+    return "Error generating this line."
+    
+if __name__ == "__main__":
+    line1 = f"{generate_line(syllable_count=5)}."
+    line2 = f"{generate_line(syllable_count=7)}."
+    line3 = f"{generate_line(syllable_count=5)}."
+
+    print_with_border([line1, line2, line3])
---
+++ modules/syllables.py
@@ -0,0 +1,60 @@
+import re as regexp
+import collections
+import cmudict
+from typing import IO
+
+pronunciations: list | None = None
+lookup: collections.defaultdict | None = None
+rhyme_lookup: collections.defaultdict | None = None
+
+# the following 3 functions were taken from the `pronouncing` module and modified to work in this script
+# i also added type hinting
+
+def _pcmu(cmufh: IO[bytes]) -> list:
+    pronunciations = list()
+    for line in cmufh:
+        line = line.strip().decode('utf-8')
+        if line.startswith(';'):
+            continue
+        word, phones = line.split(" ", 1)
+        pronunciations.append((word.split('(', 1)[0].lower(), phones))
+    return pronunciations
+
+def _rhp(phones: str) -> str:
+    phones_list = phones.split()
+    for i in range(len(phones_list) - 1, 0, -1):
+        if phones_list[i][-1] in '12':
+            return ' '.join(phones_list[i:])
+    return phones
+
+def _init_cmu(filehandle: IO[bytes] | None = None):
+    global pronunciations, lookup, rhyme_lookup
+    if pronunciations is None:
+        if filehandle is None:
+            filehandle = cmudict.dict_stream()
+        pronunciations = _pcmu(filehandle)
+        filehandle.close()
+        lookup = collections.defaultdict(list)
+        for word, phones in pronunciations:
+            lookup[word].append(phones)
+        rhyme_lookup = collections.defaultdict(list)
+        for word, phones in pronunciations:
+            rp = _rhp(phones)
+            if rp is not None:
+                rhyme_lookup[rp].append(word)
+
+def phones_for_word(find: str) -> list:
+    _init_cmu()
+    
+    if lookup is None:
+        raise ValueError("CMU Pronouncing Dictionary not initialized.")
+    return lookup.get(find.lower(), [])
+
+def get_syllables(word: str) -> int:
+    phones = phones_for_word(word.lower())
+    if phones:
+        return len(regexp.sub(r"[^012]", "", phones[0]))
+    return len(regexp.findall(r'[aeiouy]+', word.lower())) # fallback
+
+def get_line_syllables(line: str) -> int:
+    return sum(get_syllables(w) for w in regexp.findall(r'\w+', line))
